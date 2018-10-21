9fc16739f0e4a4955727ceda921a1f4f90579217
Skywt2003 <skywt2003@gmail.com>
2018-10-21 15:39:38 +0800

    Try to  solve "travel" problem

--- XJOI/CONTEST/1020/travel/travel.cpp
+++ XJOI/CONTEST/1020/travel/travel.cpp
@@ -3,6 +3,8 @@
 #include<iostream>
 #include<algorithm>
 #include<queue>
+#include<stack>
+#include<vector>
 
 using namespace std;
 #define int long long
@@ -10,10 +12,12 @@ using namespace std;
 
 const int tt=998244353;
 const int maxn=1005,maxe=2005;
-int n,a[maxn],d[maxn],f[maxn];
+int n,a[maxn],d[maxn];
 int tot=0,lnk[maxn],nxt[maxe],son[maxe],p[maxn];
-int fa[maxn];
+int f[maxn],sum[maxn];
+int g[maxn],h[maxn];
 int ans=0;
+int fa[maxn][25],deep[maxn];
 
 inline int read(){
 	int ret=0,f=1;char ch=getchar();
@@ -27,13 +31,57 @@ inline void add(int x,int y,int z){
 	nxt[tot]=lnk[x];lnk[x]=tot;
 }
 
-inline int qsm(int a,int b){
-	int ret=1,w=a;
-	while (b){
-		if (b&1) ret=ret*w%tt;
-		w=w*w%tt;b>>=1;
+inline int lowbit(int x){
+	return x&(-x);
+}
+
+namespace Tree{
+	inline void _build_tree(int x){
+		for (int i=lnk[x];i;i=nxt[i]) if (son[i]!=fa[x][0]){
+			fa[son[i]][0]=x;
+			deep[son[i]]=deep[x]+1;
+			_build_tree(son[i]);
+		}
 	}
-	return ret;
+
+	inline void build_tree(){
+		deep[1]=1;
+		_build_tree(1);
+	}
+
+	inline void _build_f(int x){
+
+	}
+
+	inline void build_f(){
+		
+	}
+}
+
+namespace LCA{
+	inline void build_lca(){
+		for (int j=1;j<25;j++)
+			for (int i=1;i<=n;i++)
+				fa[i][j]=fa[fa[i][j-1]][j-1];
+	}
+
+	inline int get_grandfa(int x,int y){ // 返回 x 的 y 级祖先
+		int ret=x;
+		while (y){
+			int delta=lowbit(y);
+			if (fa[x][delta]==0) return 1;
+			x=fa[x][delta];y-=delta;
+		}
+		return ret;
+	}
+
+	inline int get_lca(int x,int y){
+		if (deep[x]<deep[y]) swap(x,y);
+		for (int i=24;i>=0;i--) if (deep[fa[x][i]]>=deep[y]) x=fa[x][i];
+		if (x==y) return x;
+		for (int i=24;i>=0;i--) if (fa[x][i]!=fa[y][i]) x=fa[x][i],y=fa[y][i];
+		return fa[x][0];
+	}
 }
 
 signed main(){
@@ -43,6 +91,6 @@ signed main(){
 		int x=read(),y=read(),z=read();
 		add(x,y,z);add(y,x,z);
 	}
-	
+	Tree::build_tree();
 	return 0;
 }
