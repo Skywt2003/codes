bd03620a3ff25d47e98ba868f16f44edaf188c7e
Skywt2003 <skywt2003@gmail.com>
2018-10-22 22:01:36 +0800

    Try to solve OrAnd Problem

---
+++ XJOI/Level5/Sublevel-1/3855/orand.cpp
@@ -0,0 +1,38 @@
+#include<cstdio>
+#include<cstring>
+#include<iostream>
+#include<algorithm>
+
+#define int long long
+using namespace std;
+
+const int maxn=55;
+int n;
+int a[maxn],b[maxn];
+
+inline int read(){
+	int ret=0,f=1;char ch=getchar();
+	while (ch<'0'||ch>'9') {if (ch=='-') f=-1;ch=getchar();}
+	while (ch>='0'&&ch<='9') ret=ret*10+ch-'0',ch=getchar();
+	return ret*f;
+}
+
+signed main(){
+	n=read();
+	for (int i=1;i<=n;i++) a[i]=read();
+	for (int i=1;i<=n;i++) b[i]=read()-a[i];
+	for (int i=1;i<=n;i++){
+		for (int j=62;j>=0;j--){
+			int now=(int)1<<j;
+			if (((a[i]&now)==0) && (b[i]&now)){
+				printf("Impossible\n");
+				return 0;
+			} else if (((a[i]&now)==0) && (b[i-1]&now)){
+				printf("Impossible\n");
+				return 0;
+			}
+		}
+	}
+	printf("Possible\n");
+	return 0;
+}
