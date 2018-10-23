5679f0f35864fa748fbb8aa930ed20fb69aa1c65
Skywt2003 <skywt2003@gmail.com>
2018-10-23 21:40:05 +0800

    AC XJOI 3859

--- XJOI/Level5/Sublevel-1/3859/hamiltonian.cpp
+++ XJOI/Level5/Sublevel-1/3859/hamiltonian.cpp
@@ -1,86 +1,20 @@
-#pragma GCC target("avx")
-#pragma GCC optimize(3)
-#pragma GCC optimize("Ofast")
-#pragma GCC optimize("inline")
-#pragma GCC optimize("-fgcse")
-#pragma GCC optimize("-fgcse-lm")
-#pragma GCC optimize("-fipa-sra")
-#pragma GCC optimize("-ftree-pre")
-#pragma GCC optimize("-ftree-vrp")
-#pragma GCC optimize("-fpeephole2")
-#pragma GCC optimize("-ffast-math")
-#pragma GCC optimize("-fsched-spec")
-#pragma GCC optimize("unroll-loops")
-#pragma GCC optimize("-falign-jumps")
-#pragma GCC optimize("-falign-loops")
-#pragma GCC optimize("-falign-labels")
-#pragma GCC optimize("-fdevirtualize")
-#pragma GCC optimize("-fcaller-saves")
-#pragma GCC optimize("-fcrossjumping")
-#pragma GCC optimize("-fthread-jumps")
-#pragma GCC optimize("-funroll-loops")
-#pragma GCC optimize("-fwhole-program")
-#pragma GCC optimize("-freorder-blocks")
-#pragma GCC optimize("-fschedule-insns")
-#pragma GCC optimize("inline-functions")
-#pragma GCC optimize("-ftree-tail-merge")
-#pragma GCC optimize("-fschedule-insns2")
-#pragma GCC optimize("-fstrict-aliasing")
-#pragma GCC optimize("-fstrict-overflow")
-#pragma GCC optimize("-falign-functions")
-#pragma GCC optimize("-fcse-skip-blocks")
-#pragma GCC optimize("-fcse-follow-jumps")
-#pragma GCC optimize("-fsched-interblock")
-#pragma GCC optimize("-fpartial-inlining")
-#pragma GCC optimize("no-stack-protector")
-#pragma GCC optimize("-freorder-functions")
-#pragma GCC optimize("-findirect-inlining")
-#pragma GCC optimize("-fhoist-adjacent-loads")
-#pragma GCC optimize("-frerun-cse-after-loop")
-#pragma GCC optimize("inline-small-functions")
-#pragma GCC optimize("-finline-small-functions")
-#pragma GCC optimize("-ftree-switch-conversion")
-#pragma GCC optimize("-foptimize-sibling-calls")
-#pragma GCC optimize("-fexpensive-optimizations")
-#pragma GCC optimize("-funsafe-loop-optimizations")
-#pragma GCC optimize("inline-functions-called-once")
-#pragma GCC optimize("-fdelete-null-pointer-checks")
-
 #include<cstdio>
 #include<cstring>
 #include<iostream>
 #include<algorithm>
-#include<vector>
 
 #define int long long
 using namespace std;
 
 const int maxn=105;
 int n;
-bool vis[maxn];
 char a[maxn][maxn];
-int ans[maxn];
-bool done=false;
+int cnt[maxn];
 
 inline char read_ch(){
 	char ch=getchar();
 	while (ch!='.'&&ch!='-'&&ch!='+') ch=getchar();
 	return ch;
-}
-
-inline void DFS(int x,int step){
-	ans[step]=x;
-	if (done) return;
-	if (step==n){
-		done=true;
-		return;
-	}
-	for (int i=1;i<=n;i++) if ((!vis[i])&&(a[x][i]=='+')){
-		vis[i]=true;
-		DFS(i,step+1);
-		if (done) return;
-		vis[i]=false;
-	}
 }
 
 signed main(){
@@ -89,12 +23,17 @@ signed main(){
 		for (int j=1;j<=n;j++) a[i][j]=read_ch();
 	}
 	for (int i=1;i<=n;i++){
-		vis[i]=true;
-		DFS(i,1);
-		if (done) break;
-		vis[i]=false;
+		for (int j=1;j<=n;j++) cnt[i]+=(a[i][j]=='+');
 	}
-	for (int i=1;i<=n;i++) printf("%lld ",ans[i]-1);
+	int lst=-1;
+	for (int i=1;i<=n;i++){
+		int max_num=-1,k=-1;
+		for (int j=1;j<=n;j++) if ((lst==-1 || a[lst][j]=='+') && cnt[j]>max_num) max_num=cnt[j],k=j;
+		printf("%lld ",k-1);
+		cnt[k]=-1;
+		for (int j=1;j<=n;j++) if (a[j][k]=='+') cnt[j]--;
+		lst=k;
+	}
 	printf("\n");
 	return 0;
 }
