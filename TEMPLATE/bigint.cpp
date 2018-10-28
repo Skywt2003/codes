3edd31548e20f69aa2f9ceb35a34bd5bdb41d2af
Skywt2003 <skywt2003@gmail.com>
2018-10-28 21:07:24 +0800

    Update Bigint template

--- TEMPLATE/bigint.cpp
+++ TEMPLATE/bigint.cpp
@@ -3,19 +3,42 @@
 #include<bits/stdc++.h>
 
 const int maxlen=50;
+using namespace std;
 
 struct bigint{
-	int n,a[maxlen];
+	int len,a[maxlen];
 
-	bigint(){n=0;memset(a,0,sizeof(a));}
+	bigint(){len=0;memset(a,0,sizeof(a));}
 
-	bool operator ==(bigint b){
-		if (n!=b.n) return false;
-		for (int i=1;i<=n;i++) if (a[i]!=b.a[i]) return false;
+	inline void init(){
+		len=0;memset(a,0,sizeof(a));
+	}
+
+	inline bool operator ==(bigint b){
+		if (len!=b.len) return false;
+		for (int i=1;i<=len;i++) if (a[i]!=b.a[i]) return false;
 		return true;
 	}
 
-	bigint operator =(int x){
+	inline bigint operator =(int x){
+		bigint ret;
+		while (x) ret.a[++ret.len]=x%10,x/=10;
+		return ret;
+	}
 
+	inline bigint operator +(bigint b){
+		bigint c;
+		c.len=max(len,b.len);
+		for (int i=1;i<=c.len;i++){
+			c.a[i]+=a[i]+b.a[i];
+			c.a[i+1]+=c.a[i]/10;
+			c.a[i]%=10;
+		}
+		while (c.a[c.len+1]) c.len++;
+		return c;
+	}
+
+	inline bigint operator -(bigint b){
+		
 	}
 };
