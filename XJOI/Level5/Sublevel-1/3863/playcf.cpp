0bcc8f2624f3851fe3dc40a8015bd78312898fda
Skywt2003 <skywt2003@gmail.com>
2018-10-28 21:20:46 +0800

    AC XJOI 3863

--- XJOI/Level5/Sublevel-1/3863/playcf.cpp
+++ XJOI/Level5/Sublevel-1/3863/playcf.cpp
@@ -11,7 +11,6 @@ const int maxn=55,maxt=1e5+5;
 int n,T;
 int a[maxn],b[maxn],t[maxn];
 int f[maxt];
-vector<int> opt[maxt];
 
 inline int read(){
 	int ret=0,f=1;char ch=getchar();
@@ -24,17 +23,26 @@ struct problem{
 	int a,b,t;
 }p[maxn];
 
+inline int max(int x,int y){
+	return x>y?x:y;
+}
+
+inline bool cmp(problem aa,problem bb){
+	return (aa.a-aa.t*aa.b + bb.a-(aa.t+bb.t)*bb.b) > (aa.a-(aa.t+bb.t)*aa.b + bb.a-bb.t*bb.b);
+}
+
 signed main(){
 	n=read();T=read();
 	for (int i=1;i<=n;i++) p[i].a=read();
 	for (int i=1;i<=n;i++) p[i].b=read();
 	for (int i=1;i<=n;i++) p[i].t=read();
 
+	sort(p+1,p+1+n,cmp);
+	memset(f,0,sizeof(f));
+
 	for (int i=1;i<=n;i++){
-		for (int j=T;j>=p[i].t;j++){
-			vector now=opt[i];
-			
-		}
+		for (int j=T;j>=p[i].t;j--)
+			f[j]=max(f[j],f[j-p[i].t]+p[i].a-j*p[i].b);
 	}
 
 	int ans=0;
