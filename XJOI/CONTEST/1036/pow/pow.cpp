48cf9b0a389e9259e6e7aa30414c81567e2f8375
SkyWT <skywt2003@gmail.com>
2018-11-01 16:17:39 +0800

    AC XC.1036 T3

--- XJOI/CONTEST/1036/pow/pow.cpp
+++ XJOI/CONTEST/1036/pow/pow.cpp
@@ -6,8 +6,11 @@
 #define int long long
 using namespace std;
 
+const int maxn=1e6+10,N=1e6;
+
 int a,p,q,k;
 int b,l,m,c;
+int p1[maxn],p2[maxn];
 
 inline int read(){
 	int ret=0,f=1;char ch=getchar();
@@ -16,24 +19,21 @@ inline int read(){
 	return ret*f;
 }
 
-inline int qsm(int a,int b){
-	int ret=1,w=a%p;
-	while (b){
-		if (b&1) ret=(ret*w)%p;
-		b>>=1;w=w*w%p;
-	}
-	return ret;
+inline void build(){
+	p1[0]=1;
+	for (int i=1;i<N;i++) p1[i]=p1[i-1]*a%p;
+	p2[0]=1;
+	for (int i=1;i<N;i++) p2[i]=p2[i-1]*p1[N-1]%p*a%p;
 }
 
 signed main(){
-	freopen("pow.in","r",stdin);
-	freopen("pow.out","w",stdout);
 	a=read();p=read();q=read();k=read();
 	b=read();l=read();m=read();c=read();
+	build();
 	int x=0;
 	for (int i=1;i<=q;i++){
 		b=(m*b+c)%l;
-		x^=qsm(a,b);
+		x^=p1[b%N]*p2[b/N]%p;
 		if (i%k==0) printf("%lld\n",x);
 	}
 	return 0;
