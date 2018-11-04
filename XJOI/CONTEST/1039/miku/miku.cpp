b9eddef07bc71823917a90f3e598aad48cf79b1e
SkyWT <skywt2003@gmail.com>
2018-11-04 14:54:07 +0800

    AC XC.1039 T3 Miku

--- XJOI/CONTEST/1039/miku/miku.cpp
+++ XJOI/CONTEST/1039/miku/miku.cpp
@@ -3,9 +3,16 @@
 #include<iostream>
 #include<algorithm>
 #include<vector>
-
+#include<set>
 #define int long long
+
 using namespace std;
+
+const int maxn=1505;
+int n,m,a[maxn],L,R;
+
+set<int> s;
+set<int>::iterator it;
 
 inline int read(){
 	int ret=0,f=1;char ch=getchar();
@@ -14,16 +21,24 @@ inline int read(){
 	return ret*f;
 }
 
-const int maxn=1505;
-int n,m,L,R;
-int a[maxn];
+inline void make_answer(int x,int y){
+	it=s.lower_bound(x);
+	for (;(it!=s.end()) && (*it<y);){
+		swap(a[*it],a[*it+1]);
+		if ((*it-1>=1) && a[*it  ]<a[*it-1]) s.insert(*it-1);
+		if ((*it+2<=n) && a[*it+2]<a[*it+1]) s.insert(*it+1);
+		s.erase(it);it=s.lower_bound(x);
+	}
+}
 
 signed main(){
 	n=read();m=read();L=read();R=read();
 	for (int i=1;i<=n;i++) a[i]=read();
-	for (int i=1;i<=m;i++){
-		int l=read(),r=read();
-		sort(a+l,a+r+1);
+	s.clear();
+	for (int i=2;i<=n;i++) if (a[i]<a[i-1]) s.insert(i-1);
+	while (m--){
+		int x=read(),y=read();
+		make_answer(x,y);
 	}
 	for (int i=L;i<=R;i++) printf("%lld ",a[i]);
 	printf("\n");
