378c686008a5f7a1d54376598f54ae00fa415483
SkyWT <skywt2003@gmail.com>
2018-11-05 18:18:44 +0800

    Update the template of Segment Tree

--- TEMPLATE/segment_tree.cpp
+++ TEMPLATE/segment_tree.cpp
@@ -10,48 +10,48 @@ const int maxn=1e5+5;
 #define mid (((tr-tl)>>1)+tl)
 
 namespace SegmentTree{
-    int tree[maxn*4],tag[maxn*4];
+	int tree[maxn*4],tag[maxn*4];
 
-    inline void clear(){
-        memset(tree,0,sizeof(tree));
-        memset(tag,0,sizeof(tag));
-    }
+	inline void clear(){
+		memset(tree,0,sizeof(tree));
+		memset(tag,0,sizeof(tag));
+	}
 
-    inline void build_tree(int tl,int tr,int p,int* &a){
-        clear();
-        if (tl==tr){
-            tree[p]=a[tl];
-            return;
-        }
-        build_tree(tl,mid  ,ls,a);
-        build_tree(mid+1,tr,rs,a);
-        tree[p]=tree[ls]+tree[rs];
-    }
+	inline void build_tree(int tl,int tr,int p,int* &a){
+		clear();
+		if (tl==tr){
+			tree[p]=a[tl];
+			return;
+		}
+		build_tree(tl,mid  ,ls,a);
+		build_tree(mid+1,tr,rs,a);
+		tree[p]=tree[ls]+tree[rs];
+	}
 
-    inline void push_down(int tl,int tr,int p){
-        tree[ls]+=tag[p]*(mid-tl+1);
-        tree[rs]+=tag[p]*(tr-mid);
-        tag[ls]+=tag[p];tag[rs]+=tag[p];
-        tag[p]=0;
-    }
+	inline void push_down(int tl,int tr,int p){
+		tree[ls]+=tag[p]*(mid-tl+1);
+		tree[rs]+=tag[p]*(tr-mid);
+		tag[ls]+=tag[p];tag[rs]+=tag[p];
+		tag[p]=0;
+	}
 
-    inline void update(int sl,int sr,int tl,int tr,int p,int delta){
-        if (sl<=tl && tr<=sr){
-            tree[p]+=delta;
-            tag[p]+=delta;
-            return;
-        }
-        push_down(tl,tr,p);
-        if (sl  <=mid) update(sl,sr,tl,mid  ,ls,delta);
-        if (mid+1<=sr) update(sl,sr,mid+1,tr,rs,delta);
-        tree[p]=tree[ls]+tree[rs];
-    }
+	inline void update(int sl,int sr,int tl,int tr,int p,int delta){
+		if (sl<=tl && tr<=sr){
+			tree[p]+=delta*(tr-tl+1);
+			tag[p]+=delta;
+			return;
+		}
+		push_down(tl,tr,p);
+		if (sl  <=mid) update(sl,sr,tl,mid  ,ls,delta);
+		if (mid+1<=sr) update(sl,sr,mid+1,tr,rs,delta);
+		tree[p]=tree[ls]+tree[rs];
+	}
 
-    inline int query(int sl,int sr,int tl,int tr,int p){
-        if (sl<=tl && tr<=sr) return tree[p];
-        push_down(tl,tr,p);
-        int ret=0;
-        if (sl  <=mid) ret+=query(sl,sr,tl,mid  ,ls);
-        if (mid+1<=sr) ret+=query(sl,sr,mid+1,tr,rs);
-    }
+	inline int query(int sl,int sr,int tl,int tr,int p){
+		if (sl<=tl && tr<=sr) return tree[p];
+		push_down(tl,tr,p);
+		int ret=0;
+		if (sl  <=mid) ret+=query(sl,sr,tl,mid  ,ls);
+		if (mid+1<=sr) ret+=query(sl,sr,mid+1,tr,rs);
+	}
 }
