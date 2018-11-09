7778e9b41d53ef6e09739dc854afb54ae50c9dca
SkyWT <skywt2003@gmail.com>
2018-11-09 15:21:47 +0800

    AC Luogu P4779 && Upload the template of Dijkstra

--- LUOGU/P4779/dist.cpp
+++ LUOGU/P4779/dist.cpp
@@ -1,8 +1,10 @@
-// 181109
-
 #include<bits/stdc++.h>
+// #define int long long
+using namespace std;
 
-const int maxn=1005,maxe=10005;
+const int maxn=10005,maxe=500005;
+const int INF=0x7f7f7f7f,CANNOT=2147483647;
+int n,m,s;
 int tot=0,lnk[maxn],nxt[maxe],son[maxe],w[maxe];
 int dst[maxn];
 
@@ -35,4 +37,29 @@ namespace Dijkstra{
 			for (int j=lnk[now.x];j;j=nxt[j]) if (vis[son[j]]==false && dst[now.x]+w[j]<dst[son[j]]) dst[son[j]]=dst[now.x]+w[j],heap.push((node){son[j],dst[son[j]]});
 		}
 	}
+}
+
+inline int read(){
+	int ret=0,f=1;char ch=getchar();
+	while (ch<'0'||ch>'9') {if (ch=='-') f=-1;ch=getchar();}
+	while (ch>='0'&&ch<='9') ret=ret*10+ch-'0',ch=getchar();
+	return ret*f;
+}
+
+inline void add_edge(int x,int y,int z){
+	tot++;son[tot]=y;w[tot]=z;
+	nxt[tot]=lnk[x];lnk[x]=tot;
+}
+
+signed main(){
+	n=read(),m=read(),s=read();
+	for (int i=1;i<=m;i++){
+		int x=read(),y=read(),z=read();
+		add_edge(x,y,z);
+		// add_edge(y,x,z);
+	}
+	Dijkstra::Dijkstra(s);
+	for (int i=1;i<=n;i++) printf("%d ",(dst[i]==INF)?CANNOT:dst[i]);
+	printf("\n");
+	return 0;
 }
