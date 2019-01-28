d2a4a7ce275e4941d0827413c68bfbf52f5ca9fd
SkyWT <skywt2003@gmail.com>
2019-01-28 16:27:19 +0800

    AC CF447D

--- CodeForces/447/D.cpp
+++ CodeForces/447/D.cpp
@@ -1,16 +1,22 @@
-#include<bits/stdc++.h>
+#include<cstdio>
+#include<cstring>
+#include<iostream>
+#include<algorithm>
+#include<queue>
+#include<vector>
 
 using namespace std;
 
 #define int long long
 
-const int maxn=1005;
+const int maxn=1005,maxk=1000005;
+const int INF=(int)1<<60;
 
 int n,m,k,p;
 int a[maxn][maxn];
-int dt1=0,dt2=0;
+int x[maxk],y[maxk];
 
-priority_queue <int> heap1,heap2;
+priority_queue <int> heap;
 
 inline int read(){
 	int ret=0,f=1;char ch=getchar();
@@ -22,35 +28,35 @@ inline int read(){
 signed main(){
 	n=read();m=read();k=read();p=read();
 	for (int i=1;i<=n;i++)
-	for (int j=1;j<=m;j++)
-		a[i][j]=read();
+	for (int j=1;j<=m;j++) a[i][j]=read();
 	
-	while (!heap1.empty()) heap1.pop();
-	while (!heap2.empty()) heap2.pop();
-
+	while (!heap.empty()) heap.pop();
 	for (int i=1;i<=n;i++){
-		int now_sum=0;
-		for (int j=1;j<=m;j++) now_sum+=a[i][j];
-		heap1.push(now_sum);
+		int sum=0;
+		for (int j=1;j<=m;j++) sum+=a[i][j];
+		heap.push(sum);
 	}
-	for (int i=1;i<=m;i++){
-		int now_sum=0;
-		for (int j=1;j<=n;j++) now_sum+=a[j][i];
-		heap2.push(now_sum);
+	for (int i=1;i<=k;i++){
+		int now_top=heap.top();
+		x[i]=x[i-1]+now_top;
+		heap.pop();heap.push(now_top-m*p);
 	}
 
-	int ans=0;
+	while (!heap.empty()) heap.pop();
+	for (int i=1;i<=m;i++){
+		int sum=0;
+		for (int j=1;j<=n;j++) sum+=a[j][i];
+		heap.push(sum);
+	}
 	for (int i=1;i<=k;i++){
-		int x1=heap1.top(),x2=heap2.top();
-		if (x1-dt1<x2-dt2){
-			ans+=x2-dt2;
-			heap2.pop();heap2.push(x2-p*n);
-			dt1+=p;
-		} else {
-			ans+=x1-dt1;
-			heap1.pop();heap1.push(x1-p*m);
-			dt2+=p;
-		}
+		int now_top=heap.top();
+		y[i]=y[i-1]+now_top;
+		heap.pop();heap.push(now_top-n*p);
+	}
+
+	int ans=-INF;
+	for (int i=0;i<=k;i++){
+		ans=max(ans,x[i]+y[k-i]-(k-i)*i*p);
 	}
 
 	printf("%lld\n",ans);
