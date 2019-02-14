47a9ea303f40ad29de2d942da63d5563720027d9
SkyWT <skywt2003@gmail.com>
2019-02-14 19:40:12 +0800

    AC CF160E

--- CodeForces/160/D0.cpp
+++ CodeForces/160/D0.cpp
@@ -14,12 +14,13 @@ inline int read(){
 	return ret*f;
 }
 
-const int maxn=2e5+5;
+const int maxn=1e5+5;
 int n,m;
 
 struct edge{
 	int x,y,w;
 	int ans,id; // 1:none   2:at least one   3:any
+	bool toadd;
 }a[maxn];
 
 bool cmp(edge aa,edge bb){return aa.w<bb.w;}
@@ -37,37 +38,46 @@ namespace UniSet{
 		return fa[x];
 	}
 }
+namespace UniSet2{
+	int fa[maxn];
 
-int tot=0,son[maxn],nxt[maxn],lnk[maxn],vis[maxn],eid[maxn],fa[maxn],fe[maxn];
-void add_edge(int x,int y,int id){
-	tot++;eid[tot]=id;son[tot]=y;nxt[tot]=lnk[x];lnk[x]=tot;
+	void init(int n){
+		for (int i=1;i<=n;i++) fa[i]=i;
+	}
+	int getfa(int x){
+		if (fa[x]==x) return x;
+		fa[x]=getfa(fa[x]);
+		return fa[x];
+	}
+	void merge(int x,int y){
+		x=getfa(x);y=getfa(y);
+		if (x!=y) fa[x]=y;
+	}
 }
 
-void DFS(int x,int flg){
-	for (int i=lnk[x];i;i=nxt[i]) if (eid[i]!=fe[x]){
-		int nxt=UniSet::getfa(son[i]);
-		if (nxt==x) continue;
-		if (vis[nxt]!=flg && vis[nxt]!=-flg){
-			vis[nxt]=flg;
-			fa[nxt]=x;fe[nxt]=eid[i];
-			DFS(nxt,flg);
-		} else if (vis[nxt]==flg){
+int tot=0,son[maxn],nxt[maxn],lnk[maxn],vis[maxn],eid[maxn];
+void add_edge(int x,int y,int id){
+	tot++;eid[tot]=id;son[tot]=y;nxt[tot]=lnk[x];lnk[x]=tot;
+	cout<<x<<" -- "<<y<<" id= "<<a[id].id<<endl;
+}
+
+void DFS(int x,int faid){
+	for (int i=lnk[x];i;i=nxt[i]) if (eid[i]!=faid){
+		int nf=UniSet2::getfa(son[i]);
+		if (vis[nf]==0) vis[nf]=1,DFS(nf,eid[i]); else 
+		if (vis[nf]==1){
 			a[eid[i]].ans=2;
-			int now=x;
-			while (now!=nxt){
-				a[fe[now]].ans=2;
-				int nowfa=UniSet::getfa(fa[now]);
-				if (now==nowfa) break;
-				now=nowfa;
-			}
+			cout<<"ID="<<a[eid[i]].id<<" fucked"<<endl;
+			vis[nf]=-1;
+			DFS(nf,eid[i]);
 		}
 	}
-	vis[x]=-flg;
+	vis[x]=-1;
 }
 
 signed main(){
 	n=read();m=read();
-	UniSet::init(n);
+	UniSet::init(n);UniSet2::init(n);
 	for (int i=1;i<=m;i++) a[i].x=read(),a[i].y=read(),a[i].w=read(),a[i].ans=1,a[i].id=i;
 
 	sort(a+1,a+1+m,cmp);
@@ -79,27 +89,24 @@ signed main(){
 			if (fx==fy) {i++;continue;}
 			a[i].ans=3;
 			UniSet::fa[fx]=fy;
+			UniSet2::merge(a[i].x,a[i].y);
 		} else {
 			for (int k=i;k<=j;k++){
 				int fx=UniSet::getfa(a[k].x),fy=UniSet::getfa(a[k].y);
-				if (fx==fy) continue;
-				add_edge(fx,fy,k);add_edge(fy,fx,k);
+				if (fx!=fy) UniSet::fa[fx]=fy;
+				a[k].toadd=true;
 				a[k].ans=3;
-			}
-			for (int k=i;k<=j;k++){
-				int fx=UniSet::getfa(a[k].x),fy=UniSet::getfa(a[k].y);
-				if (fx==fy) continue;
-				if (vis[fx]==i || vis[fx]==-i) continue;
-				vis[fx]=i;DFS(fx,i);
-			}
-			for (int k=i;k<=j;k++){
-				int fx=UniSet::getfa(a[k].x),fy=UniSet::getfa(a[k].y);
-				if (fx==fy) continue;
-				UniSet::fa[fx]=fy;
 			}
 		}
 		i=j+1;
 	}
+	for (int i=1;i<=m;i++) if (a[i].toadd){
+		int fx=UniSet2::getfa(a[i].x),fy=UniSet2::getfa(a[i].y);
+		add_edge(fx,fy,i);add_edge(fy,fx,i);
+	}
+
+	vis[UniSet2::getfa(1)]=1;
+	DFS(UniSet2::getfa(1),-1);
 
 	sort(a+1,a+1+m,cmp_id);
 	for (int i=1;i<=m;i++){
