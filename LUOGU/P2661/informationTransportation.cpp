f94676e4f0a473be410713b0bda20a125a2a4b03
SkyWT <skywt2003@gmail.com>
2019-08-01 19:25:09 +0800

    AC Luogu P2661 informationTransportation...

--- LUOGU/P2661/informationTransportation.cpp
+++ LUOGU/P2661/informationTransportation.cpp
@@ -11,22 +11,56 @@ inline int read(){
 }
 
 const int maxn=200005;
+const int INF=1<<30;
 
-int n,to[maxn],deep[maxn];
+int n,to[maxn],f[maxn];
 
-int fa[maxn];
+int fa[maxn],cnt[maxn];
+int ans=INF;
 
+int get_fa(int x){
+    if (fa[x]==x) return x;
+    fa[x]=get_fa(fa[x]);
+    return fa[x];
+}
 
+void merge(int x,int y){
+    x=get_fa(x); y=get_fa(y);
+    if (x==y) return;
+    fa[x]=y; cnt[y]+=cnt[x]; cnt[x]=0;
+}
 
 void init(int n){
-    for (int i=1;i<=n;i++) fa[i]=i;
+    for (int i=1;i<=n;i++) fa[i]=i,cnt[i]=1;
+}
+
+queue<int> que;
+
+void Topology(){
+    while (!que.empty()) que.pop();
+    for (int i=1;i<=n;i++) if (f[i]==0) que.push(i);
+    while (!que.empty()){
+        int head=que.front(); que.pop();
+        f[to[head]]--;
+        if (f[to[head]]==0) que.push(to[head]);
+    }
+}
+
+void make_answer(){
+    init(n);
+    for (int i=1;i<=n;i++) if (f[i]) merge(i,to[i]);
+    for (int i=1;i<=n;i++) if (f[i] && cnt[i]) ans=min(ans,cnt[i]);
 }
 
 signed main(){
     n=read();
-    init(n);
     for (int i=1;i<=n;i++){
         to[i]=read();
-        int fx=get_fa(i),fy=get_fa(to[i]);
+        f[to[i]]++;
     }
+    Topology();
+    make_answer();
+
+    printf("%lld\n",ans);
+    return 0;
 }
