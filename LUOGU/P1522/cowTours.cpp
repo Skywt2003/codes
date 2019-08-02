6089610dc324c2e035309111575f3070433dd0f0
SkyWT <skywt2003@gmail.com>
2019-08-02 09:00:42 +0800

    AC Luogu P1522 cowTours

--- LUOGU/P1522/cowTours.cpp
+++ LUOGU/P1522/cowTours.cpp
@@ -16,80 +16,47 @@ char read_char(){
     return ch;
 }
 
-const int maxn=155,maxe=150*150+5;
+const int maxn=155;
 const double INF=1e9;
 
 int n,x[maxn],y[maxn];
-double ans=INF;
+double ans1=0,ans2=INF;
 
-int tot=0,lnk[maxn],nxt[maxe],to[maxe];
-double w[maxe];
-
-double g[maxn][maxn],dist[maxn];
+double lnk[maxn][maxn],dist[maxn];
+double farway[maxn];
 
 double get_dist(int i,int j){
     return sqrt(sqr(x[i]-x[j])+sqr(y[i]-y[j]));
 }
 
-bool vis[maxn];
-queue <int> que;
-
-void SPFA(int s){
-	while (!que.empty()) que.pop();
-	fill(dist,dist+maxn,INF);
-	memset(vis,0,sizeof(vis));
-
-	que.push(s); vis[s]=true; dist[s]=0.0;
-	while (!que.empty()){
-		int head=que.front(); que.pop();
-		vis[head]=false;
-		for (int i=1;i<=n;i++) if (g[head][i]!=INF && dist[head]+g[head][i]<dist[i]){
-            dist[i]=dist[head]+g[head][i];
-            if (!vis[i]) que.push(i),vis[i]=true;
-        }
-	}
+void Floyd(){
+    for (int k=1;k<=n;k++)
+        for (int i=1;i<=n;i++) if (i!=k && lnk[i][k]!=INF)
+            for (int j=1;j<=n;j++) if (k!=j && lnk[k][j]!=INF && i!=j)
+                lnk[i][j]=min(lnk[i][j],lnk[i][k]+lnk[k][j]);
 }
 
-int fa[maxn];
-
-int getfa(int x){
-    if (fa[x]==x) return x;
-    fa[x]=getfa(fa[x]);
-    return fa[x];
-}
-
-void merge(int x,int y){
-    x=getfa(x),y=getfa(y);
-    if (x==y) return;
-    fa[x]=y;
-}
-
-double get_diameter(int s){
-    SPFA(s);
-    double max_num=0.0,k=-1;
-    for (int i=1;i<=n;i++) if (dist[i]!=INF && dist[i]>max_num) max_num=dist[i],k=i;
-    SPFA(k); max_num=0.0;
-    for (int i=1;i<=n;i++) if (dist[i]!=INF) max_num=max(max_num,dist[i]);
-    return max_num;
+void make_farway(){
+    for (int i=1;i<=n;i++)
+        for (int j=1;j<=n;j++)
+            if (lnk[i][j]!=INF) farway[i]=max(farway[i],lnk[i][j]);
 }
 
 signed main(){
-    freopen("testdata.in","r",stdin);
     n=read();
-    for (int i=1;i<=n;i++) x[i]=read(),y[i]=read(),fa[i]=i;
+    for (int i=1;i<=n;i++) x[i]=read(),y[i]=read();
     for (int i=1;i<=n;i++)
-        for (int j=1;j<=n;j++){
-            g[i][j]=(read_char()=='1')?get_dist(i,j):INF;
-            if (g[i][j]!=INF) merge(i,j);
-        }
+        for (int j=1;j<=n;j++)
+            lnk[i][j]=(read_char()=='1')?get_dist(i,j):INF;
+    for (int i=1;i<=n;i++) lnk[i][i]=0.0;
 
+    Floyd();
+    make_farway();
+    for (int i=1;i<=n;i++) ans1=max(ans1,farway[i]);
     for (int i=1;i<=n;i++)
-        for (int j=i+1;j<=n;j++) if (g[i][j]==INF && getfa(i)!=getfa(j)){
-            g[i][j]=g[j][i]=get_dist(i,j);
-            ans=min(ans,get_diameter(i));
-            g[i][j]=g[j][i]=INF;
-        }
+        for (int j=1;j<=n;j++) if (lnk[i][j]==INF)
+            ans2=min(ans2,farway[i]+farway[j]+get_dist(i,j));
 
-    printf("%.6lf\n",ans);
+    printf("%.6lf\n",max(ans1,ans2));
     return 0;
 }
