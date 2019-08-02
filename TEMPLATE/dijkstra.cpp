cda86030b1b684f0a12a2f5053f00e6d8f13231a
SkyWT <skywt2003@gmail.com>
2019-08-02 09:01:07 +0800

    Update template of Dijkstra...

--- TEMPLATE/dijkstra.cpp
+++ TEMPLATE/dijkstra.cpp
@@ -18,24 +18,25 @@ struct node{
 		return w<b.w;
 	}
 };
-void make_node(int x,int w){node ret; ret.x=x; ret.w=w; return ret;}
+node make_node(int x,int w){node ret; ret.x=x; ret.w=w; return ret;}
 
 priority_queue <node> heap;
 bool vis[maxn];
 
 void Dijkstra(int s){
-	memset(dist,0x7f,sizeof(dist));
+	memset(dist,63,sizeof(dist));
 	memset(vis,0,sizeof(vis));
 	while (!heap.empty()) heap.pop();
-	dist[s]=0;vis[s]=true;
-	for (int i=lnk[s];i;i=nxt[i]) if (to[i]!=s) dist[to[i]]=min(dist[to[i]],w[i]),heap.push(make_node(to[i],dist[to[i]]));
-	for (int i=1;i<n && !heap.empty();i++){
-		node now=heap.top(); heap.pop();
-		while ((!heap.empty()) && (vis[now.x] || dist[now.x]<now.w)) now=heap.top(),heap.pop();
-		if (heap.empty() && (vis[now.x] || dist[now.x]<now.w)) break;
-		vis[now.x]=true;
-		for (int j=lnk[now.x];j;j=nxt[j])
-			if (vis[to[j]]==false && dist[now.x]+w[j]<dist[to[j]])
-				dist[to[j]]=dist[now.x]+w[j],heap.push(make_node(to[j],dist[to[j]]));
-	}
+
+	dist[s]=0.0;
+	heap.push(make_node(s,dist[s]));
+    while (!heap.empty()){
+        node now=heap.top(); heap.pop();
+        if (vis[now.x]) continue;
+        vis[now.x]=true;
+        for (int i=lnk[now.x];i;i=nxt[i]) if (dist[now.x]+w[i]<dist[to[i]]){
+            dist[to[i]]=dist[now.x]+w[i];
+            if (!vis[to[i]]) heap.push(make_node(to[i],dist[to[i]]));
+        }
+    }
 }
