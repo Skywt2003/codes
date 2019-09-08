cb0630f6c5ec13dacbcdf32b84343c9b554f2f96
SkyWT <skywt2003@gmail.com>
2019-09-08 16:35:07 +0800

    Finally AC XCdev1235 t3 caozuo

--- XJOI/CONTEST/xjoi.net/1235/caozuo.cpp
+++ XJOI/CONTEST/xjoi.net/1235/caozuo.cpp
@@ -1,5 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define int long long
 
 inline int read(){
     int ret=0,f=1;char ch=getchar();
@@ -12,12 +13,8 @@ const int tt=1e9+7;
 
 const int maxn=1000005;
 bool a[maxn];
-
-int make(int x){
-    x-=2;
-    if (x<0) return 1;
-    return 1<<x;
-}
+bool lst;
+int prem[maxn];
 
 #define ls (p<<1)
 #define rs (p<<1|1)
@@ -25,16 +22,16 @@ int make(int x){
 
 class segmentTree{
     private:
-        int sum[maxn*4];
+        int sum[maxn*4],num[maxn*4];
         bool tag[maxn*4];
-        bool num[maxn*4];
 
         void push_down(int tl,int tr,int p){
             if (tag[p]){
-                tag[ls]^=1; tag[rs]^=1;
-                num[ls]^=1; num[rs]^=1;
-                sum[ls]=(make(mid+1))-(make(tl))-sum[ls];
-                sum[rs]=(make(tr+1))-(make(mid+1))-sum[rs];
+                tag[ls]=1-tag[ls]; tag[rs]=1-tag[rs];
+                num[ls]=(mid-tl+1)-num[ls];
+                num[rs]=(tr-mid)-num[rs];
+                sum[ls]=((prem[mid+1]-prem[tl]+tt)%tt-sum[ls]+tt)%tt;
+                sum[rs]=((prem[tr+1]-prem[mid+1]+tt)%tt-sum[rs]+tt)%tt;
                 tag[p]=0;
             }
         }
@@ -42,33 +39,40 @@ class segmentTree{
     public:
         void build(int tl,int tr,int p){
             if (tl==tr){
-                if (a[tl]) sum[p]=make(tl);
+                if (a[tl]) sum[p]=prem[tl];
+                num[p]=a[tl];
                 return;
             }
             build(tl,mid,ls);
             build(mid+1,tr,rs);
-            sum[p]=sum[ls]+sum[rs];
+            sum[p]=(sum[ls]+sum[rs])%tt;
+            num[p]=num[ls]+num[rs];
         }
 
         void update(int sl,int sr,int tl,int tr,int p){
             if (sl<=tl && tr<=sr){
-                tag[p]^=1; num[p]^=1;
-                sum[p]=(make(tr+1))-(make(tl))-sum[p];
+                tag[p]=1-tag[p]; num[p]=(tr-tl+1)-num[p];
+                sum[p]=((prem[tr+1]-prem[tl]+tt)%tt-sum[p]+tt)%tt;
                 return;
             }
             push_down(tl,tr,p);
             if (sl<=mid)   update(sl,sr,tl,mid,ls);
             if (mid+1<=sr) update(sl,sr,mid+1,tr,rs);
-            sum[p]=sum[ls]+sum[rs];
+            sum[p]=(sum[ls]+sum[rs])%tt;
+            num[p]=num[ls]+num[rs];
         }
 
         int query(int sl,int sr,int tl,int tr,int p){
             if (sl<=tl && tr<=sr) return sum[p];
             push_down(tl,tr,p);
             int ret=0;
-            if (sl<=mid)   ret+=query(sl,sr,tl,mid,ls);
-            if (mid+1<=sr) ret+=query(sl,sr,mid+1,tr,rs);
+            if (sl<=mid)   ret=(ret+query(sl,sr,tl,mid,ls))%tt;
+            if (mid+1<=sr) ret=(ret+query(sl,sr,mid+1,tr,rs))%tt;
             return ret;
+        }
+
+        int check(){
+            return ((num[1]+lst)&1)==0;
         }
 };
 
@@ -79,18 +83,23 @@ segmentTree t;
 
 signed main(){
     scanf("%s",s+1); n=strlen(s+1);
-    for (int i=1;i<n/2;i++) swap(s[i],s[n-i+1]);
+
+    prem[1]=0; prem[2]=1;
+    for (int i=3;i<=n+1;i++) prem[i]=prem[i-1]*2%tt;
+
+    for (int i=1;i<=n/2;i++) swap(s[i],s[n-i+1]);
     for (int i=1;i<=n;i++) a[i]=s[i]=='1';
 
+    lst=a[1];a[1]=0;
     t.build(1,n,1);
-    printf("%d\n",t.query(1,n,1,n,1));
+    printf("%lld\n",(t.query(2,n,1,n,1)+(int)lst+t.check()-(t.check()&&lst)+tt)%tt);
 
     m=read();
     while (m--){
         int x=read(),y=read();
-        if (x>y) swap(x,y); if (x<1) x=1; if (y>n) y=n;
+        if (x>y) swap(x,y); if (x<1) x=1; if (y>=n) y=n-1,lst=1-lst;
         t.update(n-y+1,n-x+1,1,n,1);
-        printf("%d\n",t.query(1,n,1,n,1));
+        printf("%lld\n",(t.query(2,n,1,n,1)+(int)lst+t.check()-(t.check()&&lst)+tt)%tt);
     }
     return 0;
 }
