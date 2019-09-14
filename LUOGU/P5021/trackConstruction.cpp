da1d056e725ab77ed087bc64ba9a1d18187bb0cd
SkyWT <skywt2003@gmail.com>
2019-09-14 18:24:02 +0800

    AC Luogu P5021 trackConstruction with O2

--- LUOGU/P5021/trackConstruction.cpp
+++ LUOGU/P5021/trackConstruction.cpp
@@ -1,7 +1,8 @@
 #include<bits/stdc++.h>
+
 using namespace std;
 
-#define int long long
+// #define int long long
 
 inline int read(){
     int ret=0,f=1;char ch=getchar();
@@ -26,33 +27,38 @@ int cnt=0;
 int L=0,R=0,ans=-1;
 
 int DFS(int x,int fa,int k){
-    printf("Start to DFS: %lld\n",x);
+    // printf("Start to DFS: %d\n",x);
     multiset<int> s; s.clear();
-    multiset<int>::iterator it,found;
+    multiset<int>::iterator it,found,nxtit;
     for (int i=lnk[x];i;i=nxt[i]) if (to[i]!=fa){
-        int nxt=DFS(to[i],x,k)+w[i];
-        if (nxt>=k) cnt++; else s.insert(nxt);
+        int nxtx=DFS(to[i],x,k)+w[i];
+        if (nxtx>=k) cnt++; else s.insert(nxtx);
     }
     if (s.empty()) return 0;
     
-    printf("now:");for (it=s.begin();it!=s.end();it++) printf("%lld ",(*it)); printf("\n");
+    // printf("now:");for (it=s.begin();it!=s.end();it++) printf("%d ",(*it)); printf("\n");
 
-    for (it=s.begin();(it!=s.end())&&(!s.empty());it++){
+    for (it=s.begin();(it!=s.end())&&(s.size()>1);/*it++*/){
         found=s.lower_bound(k-(*it));
-        if (found==s.end()) continue;
+        if (found==s.end()) {it++;continue;}
         if (found==it){
             found++;
-            if (found==s.end()) continue;
+            if (found==s.end()) {it++;continue;}
         }
+        nxtit=it; nxtit++;
+        if (nxtit==found) nxtit++;
         s.erase(it); s.erase(found); cnt++;
+        it=nxtit;
     }
-    printf("Ending DFS: %lld\n",x);
+    // printf("Ending DFS: %d\n",x);
     if (s.empty()) return 0;
-    return *(s.rbegin());
+    int ret=*(s.rbegin());
+    // multiset<int>().swap(s);
+    return ret;
 }
 
 bool check(int k){
-    printf("Start to check mid=%lld\n",mid);
+    // printf("Start to check mid=%d\n",mid);
     cnt=0;
     DFS(1,-1,k);
     return cnt>=m;
@@ -67,6 +73,6 @@ signed main(){
 
     while (L<=R) if (check(mid)) ans=mid,L=mid+1; else R=mid-1;
 
-    printf("%lld\n",ans);
+    printf("%d\n",ans);
     return 0;
 }
