fdb498972ec4cd2435005038c625ed4841b5b559
SkyWT <skywt2003@gmail.com>
2019-09-18 20:26:03 +0800

    AC Luogu P3758 cola && update matrix tmp

--- LUOGU/P3758/cola.cpp
+++ LUOGU/P3758/cola.cpp
@@ -1,9 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int tt=19260817;
+
+inline int read(){
+    int ret=0,f=1;char ch=getchar();
+    while (ch<'0'||ch>'9') {if (ch=='-') f=-1;ch=getchar();}
+    while (ch>='0'&&ch<='9') ret=ret*10+ch-'0',ch=getchar();
+    return ret*f;
+}
+
+const int maxn=35,maxt=1e6+5;
+const int tt=2017;
+
+int n,e,t;
 
 class matrix{
-	static const int maxn=55;
 	private:
 		int n,m,a[maxn][maxn];
 
@@ -26,6 +36,17 @@ class matrix{
 			memset(a,0,sizeof(a));
 			for (int i=0;i<min(n,m);i++) a[i][i]=1;
 		}
+
+        void set_init(int x,int y,int v){
+            n=x;m=y;
+            for (int i=0;i<n;i++)
+                for (int j=0;j<m;j++)
+                    a[i][j]=v;
+        }
+
+        int get(int x,int y){
+            return a[x][y];
+        }
 
 		bool operator ==(matrix &b){
 			if (n!=b.n || m!=b.m) return false;
@@ -93,3 +114,25 @@ class matrix{
 			return ret;
 	}
 };
+
+matrix trans;
+int ans=0;
+
+signed main(){
+    n=read(); e=read();
+    n++;
+    trans.set_unit(n,n);
+    for (int i=0;i<e;i++){
+        int x=read(),y=read();
+        trans.set_value(x,y,1);
+        trans.set_value(y,x,1);
+    }
+    for (int i=1;i<n;i++) trans.set_value(i,0,1);
+    t=read();
+
+    trans=trans^t;
+    int ans=0;
+    for (int i=0;i<n;i++) ans=(ans+trans.get(1,i))%tt;
+    printf("%d\n",ans);
+    return 0;
+}
