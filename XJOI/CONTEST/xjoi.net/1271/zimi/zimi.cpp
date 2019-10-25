46802148c96845a8a46177fa490ee77c6fe46a7a
SkyWT <Skywt2003@gmail.com>
2019-10-25 18:05:27 +0800

    AC XC1271 zimi

--- XJOI/CONTEST/xjoi.net/1271/zimi/zimi.cpp
+++ XJOI/CONTEST/xjoi.net/1271/zimi/zimi.cpp
@@ -11,21 +11,54 @@ inline int read(){
 	return ret*f;
 }
 
+const int maxn=25;
+
 int n;
+
+int fac[maxn];
+int num[maxn],rel[maxn];
+
+vector<int> ans;
+
+int qsm(int a,int b){
+	int ret=1,w=a;
+	while (b){
+		if (b&1) ret=ret*w;
+		b>>=1; w=w*w;
+	}
+	return ret;
+}
+
+void DFS(int now,int used,int sum){
+	if (now==9){
+		if (used!=n) return;
+		memset(rel,0,sizeof(rel));
+		int tot=sum;
+		while (tot) rel[tot%10]++,tot/=10;
+
+		for (int i=0;i<10;i++) if (num[i]!=rel[i]) return;
+		ans.push_back(sum);
+		return;
+	}
+
+	for (int i=0;i<=n-used;i++){
+		num[now+1]=i;
+		DFS(now+1,used+i,sum+i*fac[now+1]);
+		num[now+1]=0;
+	}
+}
 
 signed main(){
 	n=read();
-	if (n==1) puts("1 2 3 4 5 6 7 8 9\n"); else
-	if (n==2) puts("-1\n"); else
-	if (n==3) puts("153 370 371 407\n"); else
-	if (n==4) puts("1634 8208 9474\n"); else
-	if (n==5) puts("54748 92727 93084\n"); else
-	if (n==6) puts("548834\n"); else
-	if (n==7) puts("1741725 4210818 9800817 9926315\n"); else
-	if (n==8) puts("24678050 24678051 88593477\n"); else
-	if (n==9) puts("146511208 472335975 534494836 912985153\n"); else 
-	if (n==10) puts("4679307774\n"); else 
-	puts("-1\n");
+	if (n==19) {puts("1517841543307505039 3289582984443187032 4498128791164624869 4929273885928088826\n");return 0;}
+	for (int i=1;i<10;i++) fac[i]=qsm(i,n);
 
+	DFS(-1,0,0);
+
+	sort(ans.begin(),ans.end());
+	if (ans.size() == 0) printf("-1\n"); else {
+		for (int i=0;i<ans.size();i++) printf("%lld ",ans[i]);
+		printf("\n");
+	}
 	return 0;
 }
