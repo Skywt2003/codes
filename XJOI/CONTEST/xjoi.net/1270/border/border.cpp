9d56edb61d6e8536200076f71465bbdbbb36226e
SkyWT <Skywt2003@gmail.com>
2019-10-25 21:13:32 +0800

    AC XC1270T3 border

--- XJOI/CONTEST/xjoi.net/1270/border/border.cpp
+++ XJOI/CONTEST/xjoi.net/1270/border/border.cpp
@@ -14,34 +14,72 @@ inline int read(){
 const int maxn=5e5+5;
 const int tt=1e9+7;
 
-int n,q;
+int n,m;
 int a[maxn];
-int sum[maxn];
-int ans[maxn];
+int sum[maxn],suf[maxn],fin[maxn];
 
-int qsm(int a,int b){
-	if (b<0) return 0;
-	int ret=1,w=a%tt;
-	while (b){
-		if (b&1) ret=ret*w%tt;
-		b>>=1; w=w*w%tt;
-	}
-	return ret;
+int qsm2[maxn],pre[maxn];
+
+int fa[maxn];
+
+int getfa(int x){
+	return (fa[x]==x)?x:fa[x]=getfa(fa[x]);
+}
+
+void merge(int x,int y){
+	fa[x]=y; pre[y]=pre[x];
+	int len=x-pre[x];
+	if (len>30 && sum[y]>0 || sum[x]+(sum[y]<<len)>tt) sum[y]=tt; else
+	sum[y]=sum[x]+(sum[y]<<len);
+}
+
+struct query{
+	int x,y,ans,id;
+}q[maxn];
+
+bool compare_query_y(query aa,query bb){
+	return aa.y < bb.y;
+}
+bool compare_query_id(query aa,query bb){
+	return aa.id < bb.id;
+}
+
+void pre_build(){
+	qsm2[0]=1;
+	for (int i=1;i<=n;i++)
+		qsm2[i]=qsm2[i-1]*2%tt,
+		pre[i]=i-1,
+		sum[i]=a[i],
+		fa[i]=i;
+	for (int i=n;i>=1;i--) suf[i]=(suf[i+1]*2%tt+a[i]+tt)%tt;
+}
+
+int get_part(int x,int y){
+	return (suf[x] - suf[y+1]*qsm2[y-x+1]%tt + tt)%tt;
 }
 
 signed main(){
 	#ifdef DEBUG
 		freopen("data.in","r",stdin);
 	#endif
-	n=read(); q=read();
+	n=read(); m=read();
 	for (int i=1;i<=n;i++) a[i]=read();
-	int tmp=1;
-	for (int i=1;i<=n;i++) sum[i]=(sum[i-1]+a[i])%tt,ans[i]=(ans[i-1]+a[i]*tmp%tt)%tt,tmp=tmp*2%tt;
+	for (int i=1;i<=m;i++) q[i].x=read(),q[i].y=read(),q[i].id=i;
+	sort(q+1,q+1+m,compare_query_y);
 	
-	while (q--){
-		int x=read(),y=read();
-		printf("%lld\n",(ans[y]-ans[x-1]+tt)%tt * qsm(qsm(2,x-1),tt-2)%tt);
+	pre_build();
+	int j=0;
+	for (int i=1;i<=n;i++){
+		while (pre[i] && sum[i]>=0) merge(pre[i],i);
+		fin[i]=(fin[pre[i]]+get_part(pre[i]+1,i)*2%tt)%tt;
+		while (q[j+1].y == i){
+			j++; int x=getfa(q[j].x);
+			q[j].ans=(fin[i]-fin[x]+get_part(q[j].x,x)+tt)%tt;
+		}
 	}
 
+	sort(q+1,q+1+m,compare_query_id);
+
+	for (int i=1;i<=m;i++) printf("%lld\n",q[i].ans);
 	return 0;
 }
