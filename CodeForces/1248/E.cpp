38d7bf8902a11c00056566ca55af93e8ecc65123
SkyWT <skywt2003@gmail.com>
2019-11-04 19:06:51 +0800

    AC CF1248E

--- CodeForces/1248/E.cpp
+++ CodeForces/1248/E.cpp
@@ -2,6 +2,7 @@
 #define int long long
 
 using namespace std;
+#define pii pair<int,int>
 
 inline int read(){
 	int ret=0,f=1;char ch=getchar();
@@ -14,88 +15,25 @@ const int maxn=1e5+5;
 
 int n,p,ans[maxn];
 
-struct node{
-	int t,id;
-
-	bool operator <(node bb)const{
-		return id > bb.id;
-	}
-	bool operator >(node bb)const{
-		return id < bb.id;
-	}
-};
-
-node b[maxn];
-deque<node> que;
-priority_queue<node> heap;
-
-bool compare_s(node aa,node bb){
-	return (aa.t<bb.t)||(aa.t==bb.t && aa.id<bb.id);
-}
-
-class indexTree{
-	private:
-		int tree[maxn];
-
-		int lowbit(int x){
-			return x&(-x);
-		}
-
-	public:
-		indexTree(){
-			memset(tree,0,sizeof(tree));
-		}
-
-		void update(int x,int delta){
-			while (x<=n) tree[x]+=delta,x+=lowbit(x);
-		}
-
-		int query(int x){
-			int ret=0;
-			while (x) ret+=tree[x],x-=lowbit(x);
-			return ret;
-		}
-};
-indexTree t;
+priority_queue<pii,vector<pii>,greater<pii> > heap; // pair<time,id>
+queue<int> que;
+set<int> want;
 
 signed main(){
 	n=read(),p=read();
-	for (int i=1;i<=n;i++) b[i]=(node){read(),i};
-
-	sort(b+1,b+1+n,compare_s);
-	while (!heap.empty()) heap.pop();
-	while (!que.empty()) que.pop_back();
+	for (int i=1;i<=n;i++) heap.push(make_pair(read(),i));
 
 	int nowt=0;
 	for (int i=1;i<=n;i++){
-		while (!que.empty() && que.front().t+p <= b[i].t){
-			nowt=max(nowt+p, que.front().t+p);
-			ans[que.front().id]=nowt,
-			t.update(que.front().id,1),
-			que.pop_front();
+		if (que.empty() && want.empty()) nowt=heap.top().first;
+		nowt+=p;
+		while (!heap.empty() && heap.top().first <= nowt){
+			if (que.empty() || que.back() > heap.top().second) que.push(heap.top().second);
+			else want.insert(heap.top().second);
+			heap.pop();
 		}
-		while (!heap.empty() && t.query(heap.top().id-1)==0)
-			que.push_back(heap.top()),heap.pop();
-		if (t.query(b[i].id-1) == 0)
-			que.push_back(b[i]),
-			t.update(b[i].id,-1);
-		else heap.push(b[i]);
-	}
-
-	while ((!heap.empty()) || (!que.empty())){
-		if (!que.empty()){
-			nowt=max(nowt+p,que.front().t+p);
-			ans[que.front().id]=nowt;
-			t.update(que.front().id,1);
-			que.pop_front();
-		}
-		#ifdef EBUG
-			if (!heap.empty())
-				printf("TOP QUERY: %lld\n",t.query(heap.top().id-1));
-			else printf("Heap empty\n");
-		#endif
-		while (!heap.empty() && t.query(heap.top().id-1)==0)
-			que.push_back(heap.top()),heap.pop();
+		ans[que.front()]=nowt; que.pop();
+		if (que.empty() && !want.empty()) que.push(*want.begin()),want.erase(want.begin());
 	}
 
 	for (int i=1;i<=n;i++) printf("%lld ",ans[i]);
